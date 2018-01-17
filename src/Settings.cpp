d2502bd8017ab0182221989a369fd8faa75dca33
Nephatiu <thomas.nico@gmail.com>
2018-01-17 05:34:53 +0100

    Ghosts screwing up files

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -3,7 +3,7 @@
 // Distributed under the MIT/X11 software license,
 // see http://www.opensource.org/licenses/mit-license.php.
 
-#include <QCoreApplication>
+#include <QApplication>
 #include <QFile>
 #include <QJsonArray>
 #include <QJsonDocument>
@@ -19,8 +19,6 @@
 
 namespace WalletGui {
 
-Q_DECL_CONSTEXPR char OPTION_WALLET_FILE[] = "walletFile";
-Q_DECL_CONSTEXPR char OPTION_ENCRYPTED[] = "encrypted";
 Q_DECL_CONSTEXPR char OPTION_MINING_POOLS[] = "miningPools";
 
 Settings& Settings::instance() {
@@ -44,10 +42,10 @@ void Settings::load() {
   if (cfgFile.open(QIODevice::ReadOnly)) {
     m_settings = QJsonDocument::fromJson(cfgFile.readAll()).object();
     cfgFile.close();
-    if (!m_settings.contains(OPTION_WALLET_FILE)) {
+    if (!m_settings.contains("walletFile")) {
       m_addressBookFile = getDataDir().absoluteFilePath(QCoreApplication::applicationName() + ".addressbook");
     } else {
-      m_addressBookFile = m_settings.value(OPTION_WALLET_FILE).toString();
+      m_addressBookFile = m_settings.value("walletFile").toString();
       m_addressBookFile.replace(m_addressBookFile.lastIndexOf(".wallet"), 7, ".addressbook");
     }
   } else {
@@ -56,7 +54,7 @@ void Settings::load() {
 
   QStringList defaultPoolList;
   //defaultPoolList << "xdn.miner.center:4555" << "duckpool.mooo.com:2222" << "xdn.poolto.be:11010";
-  defaultPoolList << "0.0.0.0:4333";
+  defaultPoolList << "windvast.ggtm.eu:48137";
   if (!m_settings.contains(OPTION_MINING_POOLS)) {
     setMiningPoolList(QStringList() << defaultPoolList);
   } else {
@@ -128,7 +126,7 @@ QDir Settings::getDataDir() const {
 }
 
 QString Settings::getWalletFile() const {
-  return m_settings.contains(OPTION_WALLET_FILE) ? m_settings.value(OPTION_ENCRYPTED).toString() :
+  return m_settings.contains("walletFile") ? m_settings.value("walletFile").toString() :
     getDataDir().absoluteFilePath(QCoreApplication::applicationName() + ".wallet");
 }
 
@@ -137,7 +135,7 @@ QString Settings::getAddressBookFile() const {
 }
 
 bool Settings::isEncrypted() const {
-  return m_settings.contains(OPTION_WALLET_FILE) ? m_settings.value(OPTION_ENCRYPTED).toBool() : false;
+  return m_settings.contains("encrypted") ? m_settings.value("encrypted").toBool() : false;
 }
 
 QString Settings::getVersion() const {
@@ -183,9 +181,9 @@ bool Settings::isStartOnLoginEnabled() const {
   res = QFile::exists(autorunFilePath);
 #elif defined(Q_OS_WIN)
   QSettings autorunSettings("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", QSettings::NativeFormat);
-  QString keyName = QString("%1Wallet").arg(CurrencyAdapter::instance().getCurrencyDisplayName());
+  QString keyName = QString("%1-qt").arg(CurrencyAdapter::instance().getCurrencyDisplayName());
   res = autorunSettings.contains(keyName) &&
-    !QDir::fromNativeSeparators(autorunSettings.value(keyName).toString()).compare(QCoreApplication::applicationFilePath());
+    !QDir::fromNativeSeparators(autorunSettings.value(keyName).toString()).compare(QApplication::applicationFilePath());
 #endif
   return res;
 }
@@ -201,20 +199,20 @@ bool Settings::isCloseToTrayEnabled() const {
 #endif
 
 void Settings::setWalletFile(const QString& _file) {
-  if (_file.endsWith(".wallet") || _file.endsWith(".keys")) {
-    m_settings.insert(OPTION_WALLET_FILE, _file);
+  if (_file.endsWith(".wallet") || _file.endsWith(".keys") || _file.endsWith(".ncoin")) {
+    m_settings.insert("walletFile", _file);
   } else {
-    m_settings.insert(OPTION_WALLET_FILE, _file + ".wallet");
+    m_settings.insert("walletFile", _file + ".wallet");
   }
 
   saveSettings();
-  m_addressBookFile = m_settings.value(OPTION_WALLET_FILE).toString();
+  m_addressBookFile = m_settings.value("walletFile").toString();
   m_addressBookFile.replace(m_addressBookFile.lastIndexOf(".wallet"), 7, ".addressbook");
 }
 
 void Settings::setEncrypted(bool _encrypted) {
   if (isEncrypted() != _encrypted) {
-    m_settings.insert(OPTION_ENCRYPTED, _encrypted);
+    m_settings.insert("encrypted", _encrypted);
     saveSettings();
   }
 }
@@ -232,7 +230,7 @@ void Settings::setStartOnLoginEnabled(bool _enable) {
   QString autorunFilePath = autorunDir.absoluteFilePath(QCoreApplication::applicationName() + ".plist");
   QSettings autorunSettings(autorunFilePath, QSettings::NativeFormat);
   autorunSettings.setValue("Label", "org." + QCoreApplication::applicationName());
-  autorunSettings.setValue("Program", QCoreApplication::applicationFilePath());
+  autorunSettings.setValue("Program", QApplication::applicationFilePath());
   autorunSettings.setValue("RunAtLoad", _enable);
   autorunSettings.setValue("ProcessType", "InterActive");
 #elif defined(Q_OS_LINUX)
@@ -260,7 +258,7 @@ void Settings::setStartOnLoginEnabled(bool _enable) {
     autorunFile.write("[Desktop Entry]\n");
     autorunFile.write("Type=Application\n");
     autorunFile.write(QString("Name=%1 Wallet\n").arg(CurrencyAdapter::instance().getCurrencyDisplayName()).toLocal8Bit());
-    autorunFile.write(QString("Exec=%1\n").arg(QCoreApplication::applicationFilePath()).toLocal8Bit());
+    autorunFile.write(QString("Exec=%1\n").arg(QApplication::applicationFilePath()).toLocal8Bit());
     autorunFile.write("Terminal=false\n");
     autorunFile.write("Hidden=false\n");
     autorunFile.close();
@@ -269,9 +267,9 @@ void Settings::setStartOnLoginEnabled(bool _enable) {
   }
 #elif defined(Q_OS_WIN)
   QSettings autorunSettings("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", QSettings::NativeFormat);
-  QString keyName = QString("%1Wallet").arg(CurrencyAdapter::instance().getCurrencyDisplayName());
+  QString keyName = QString("%1-qt").arg(CurrencyAdapter::instance().getCurrencyDisplayName());
   if (_enable) {
-    autorunSettings.setValue(keyName, QDir::toNativeSeparators(QCoreApplication::applicationFilePath()));
+    autorunSettings.setValue(keyName, QDir::toNativeSeparators(QApplication::applicationFilePath()));
   } else {
     autorunSettings.remove(keyName);
   }
